94e639d7f9cc4d0f26600ca1455219edb77a72a4
mtnsaha <58302068+mtnsaha@users.noreply.github.com>
2023-05-28 00:04:49 +0530

    first commting

---
+++ src/main/java/com/dashboards/hysdashboard/HysDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.dashboards.hysdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HysDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HysDashboardApplication.class, args);
+	}
+
+}
